346c242b43612cd715e52d06b52339d3f70a3e58
Sudeep Kumar Dalei <sudeepdalei38@gmail.com>
2026-04-15 23:07:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  // Check the leap year conditions
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366;
+  } else {
+    return 365;
+  }
+}
